59f03d3e7c2c7527e290916dae07d8f3de9990dc
lqchuong <lqchuong2402@gmail.com>
2022-10-22 11:33:14 +0700

    fix size marker on android

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:interactive_diary/constants/dimens.dart';
 import 'package:nartus_ui_package/nartus_ui.dart';
 import 'package:interactive_diary/bloc/location/location_bloc.dart';
 import 'package:interactive_diary/generated/l10n.dart';
+import 'dart:ui' as ui;
 
 class IDHome extends StatefulWidget {
   const IDHome({
@@ -15,19 +19,31 @@ class IDHome extends StatefulWidget {
 }
 
 class _IDHomeState extends State<IDHome> with WidgetsBindingObserver {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   bool isAnimation = false;
   late Future<List<Marker>> futureListMarker;
+
+  Future<Uint8List?> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+        ?.buffer
+        .asUint8List();
+  }
 
   Future<List<Marker>> generateListMarkers(
       double latitude, double longitude) async {
     List<Marker> markers = [];
+    final Uint8List? markerIconClose =
+        await getBytesFromAsset('assets/images/marker_nonetap.png', 120);
+    final Uint8List? markerIconOpen =
+        await getBytesFromAsset('assets/images/marker_ontap.png', 120);
     final BitmapDescriptor icon = isAnimation == true
-        ? await BitmapDescriptor.fromAssetImage(
-            const ImageConfiguration(size: Size(24, 24)),
-            'assets/images/marker_ontap.png')
-        : await BitmapDescriptor.fromAssetImage(
-            const ImageConfiguration(size: Size(24, 24)),
-            'assets/images/marker_nonetap.png');
+        ? BitmapDescriptor.fromBytes(markerIconClose!)
+        : BitmapDescriptor.fromBytes(markerIconOpen!);
 
     final Marker marker = Marker(
         markerId: MarkerId(latitude.toString()),
